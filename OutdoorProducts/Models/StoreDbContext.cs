6d61282e51f14a9b73781d3871bbb29bd87c22a2
Connie Uribe Chavez <connie.uribechavez@outlook.com>
2020-09-09 13:51:15 -0400

    Made a few more changes to set up for the DB

---
+++ OutdoorProducts/Models/StoreDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace OutdoorProducts.Models
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options)
+            : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
