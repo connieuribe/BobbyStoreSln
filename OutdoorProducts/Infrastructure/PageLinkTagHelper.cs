70706d41307db3e79ac49f7977376d6a5c55cf79
Connie Uribe Chavez <connie.uribechavez@outlook.com>
2020-09-19 11:40:00 -0400

    Added a car Feature and more UnitTests

--- OutdoorProducts/Infrastructure/PageLinkTagHelper.cs
+++ OutdoorProducts/Infrastructure/PageLinkTagHelper.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using OutdoorProducts.Models.ViewModels;
+using System.Collections.Generic;
+
 namespace OutdoorProducts.Infrastructure
 {
     [HtmlTargetElement("div", Attributes = "page-model")]
@@ -19,6 +21,11 @@ namespace OutdoorProducts.Infrastructure
         public ViewContext ViewContext { get; set; }
         public PagingInfo PageModel { get; set; }
         public string PageAction { get; set; }
+        
+        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+        public Dictionary<string, object> PageUrlValues { get; set; } 
+            = new Dictionary<string, object>();
+
         public bool PageClassesEnabled { get; set; } = false;
         public string PageClass { get; set; }
         public string PageClassNormal { get; set; }
@@ -31,6 +38,8 @@ namespace OutdoorProducts.Infrastructure
             for (int i = 1; i <= PageModel.TotalPages; i++)
             {
                 TagBuilder tag = new TagBuilder("a");
+                PageUrlValues["productPage"] = i;
+                tag.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
                 tag.Attributes["href"] = urlHelper.Action(PageAction,
                     new { productPage = i });
                 if (PageClassesEnabled)
--- OutdoorProducts/Startup.cs
+++ OutdoorProducts/Startup.cs
@@ -21,32 +21,38 @@ namespace OutdoorProducts
             Configuration = config;
         }
         private IConfiguration Configuration { get; set; }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddDbContext<StoreDbContext>(opts =>
-            {
+            services.AddDbContext<StoreDbContext>(opts => {
                 opts.UseSqlServer(
-                    Configuration["ConnectionStrings:SportsStoreConnection"]);
+                Configuration["ConnectionStrings:SportsStoreConnection"]);
             });
             services.AddScoped<IStoreRepository, EFStoreRepository>();
+            services.AddRazorPages();
+            services.AddDistributedMemoryCache();
+            services.AddSession();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
-
+            app.UseSession();
             app.UseRouting();
             app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute("catpage",
+                "{category}/Page{productPage:int}",
+                new { Controller = "Home", action = "Index" });
+                endpoints.MapControllerRoute("page", "Page{productPage:int}",
+                new { Controller = "Home", action = "Index", productPage = 1 });
+                endpoints.MapControllerRoute("category", "{category}",
+                new { Controller = "Home", action = "Index", productPage = 1 });
                 endpoints.MapControllerRoute("pagination",
                 "Products/Page{productPage}",
-                new { Controller = "Home", action = "Index" });
+                new { Controller = "Home", action = "Index", productPage = 1 });
                 endpoints.MapDefaultControllerRoute();
+                endpoints.MapRazorPages();
             });
             SeedData.EnsurePopulated(app);
         }
